0472ce1ebccd26a0ce9381d14ad3468ef0796aba
sev3e3e <fc2tokaqq+sev3e3e@gmail.com>
2023-02-15 09:23:00 +0900

    rootは204を返すようにする

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-import { Controller, Param, Body, Get, Post, Put, Delete, JsonController } from 'routing-controllers';
+import { Get, JsonController } from 'routing-controllers';
 
 @JsonController()
 export class IndexController {
     @Get('/')
     get() {
-        return 'index.?';
+        return null;
     }
 }
