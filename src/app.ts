6574a76004ae886a0616e7fe8ad18bce4c87791b
sev3e3e <fc2tokaqq+sev3e3e@gmail.com>
2023-03-04 00:50:22 +0900

    RateLimitにRedisを使うようにする

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,7 @@ import 'reflect-metadata';
 import { IndexController } from './routes';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
-// import { createServer } from 'https';
-// import { readFileSync } from 'fs';
+import RedisStore from 'rate-limit-redis';
 import { useExpressServer } from 'routing-controllers';
 import { SchedulesController } from './routes/schedules';
 import { RedisClient } from './redisClient';
@@ -22,12 +21,17 @@ const app = express();
 
 app.disable('x-powered-by');
 
+await RedisClient.connect();
+
 // 100 requests per 10 minutes.
 const rateLimitter = rateLimit({
     windowMs: 10 * 60 * 1000,
     max: 100,
     standardHeaders: true,
     legacyHeaders: false,
+    store: new RedisStore({
+        sendCommand: (...args: string[]) => RedisClient.sendCommand(args),
+    }),
 });
 
 app.use(rateLimitter);
@@ -37,33 +41,6 @@ useExpressServer(app, {
     development: false,
 });
 
-// const TLS_KEY = process.env.TLS_KEY_PATH
-//     ? readFileSync(process.env.TLS_KEY_PATH)
-//     : process.env.TLS_KEY
-//     ? process.env.TLS_KEY.replace(/\\n/g, '\n')
-//     : '';
-
-// const TLS_CERT = process.env.TLS_CERT_PATH
-//     ? readFileSync(process.env.TLS_CERT_PATH)
-//     : process.env.TLS_CERT
-//     ? process.env.TLS_CERT.replace(/\\n/g, '\n')
-//     : '';
-
-// https
-// const tlsServer = createServer(
-//     {
-//         key: TLS_KEY,
-//         cert: TLS_CERT,
-//     },
-//     app
-// );
-
-await RedisClient.connect();
-
 app.listen(port, host, () => {
     console.log(`⚡️ Server is running on http://localhost:${port}`);
 });
-
-// tlsServer.listen(443, () => {
-//     console.log('⚡️ TLS Server is running! you can access https://localhost:443');
-// });
